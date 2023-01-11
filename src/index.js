9b7833caecafc88f66c86c7a4ad4306971761cdf
Dzmitry Pauliukevich <dmitripavluk@gmail.com>
2023-01-12 00:08:45 +0300

    feat: to do task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+
+    let NumToStr = String(Math.abs(n));
+    let strResult = '';
+
+    for (i = 0; i < NumToStr.length; i += 1) {
+        strResult = `${NumToStr[i]}${strResult}`;
+    }
+    return Number(strResult);
+
+};
+
+
+
+
+
